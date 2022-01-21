e21b5a79c5c9a0196bf48d634be90b4f1446d078
alilaode <laode@code.id>
2022-01-21 22:10:52 +0700

    Update env

--- main.go
+++ main.go
@@ -5,16 +5,9 @@ import (
 	"codeid-boiler/internal/factory"
 	"codeid-boiler/internal/http"
 	"codeid-boiler/internal/middleware"
-	"context"
-	echopprof "github.com/hiko1129/echo-pprof"
 	"github.com/labstack/echo/v4"
 	"github.com/sirupsen/logrus"
-	netHTTP "net/http"
 	"os"
-	"os/signal"
-	"strings"
-	"syscall"
-	"time"
 )
 
 /*
@@ -47,52 +40,11 @@ func main() {
 
 	var PORT = os.Getenv("PORT")
 
-	db.Init()
-	defer db.Close()
-	//migration.Init()
-	//elasticsearch.Init()
-
-	e := echo.New()
-	middleware.Init(e)
-
-	f := factory.NewFactory()
-	http.Init(e, f)
-
-	debugpprof := strings.ToLower(strings.TrimSpace(os.Getenv("DEBUG_PPROF")))
-	if debugpprof == "active" {
-		// automatically add routers for net/http/pprof
-		// e.g. /debug/pprof, /debug/pprof/heap, etc.
-		echopprof.Wrap(e)
-		logrus.Info("ECHOPPROF registered")
-	}
-	logrus.Info("DEBUG_PPROF ", debugpprof == "active", " "+debugpprof)
-
-	// Start server
-	go func() {
-		if err := e.Start(":" + PORT); err != nil && err != netHTTP.ErrServerClosed {
-			e.Logger.Fatal("shutting down the server")
-		}
-	}()
-
-	// Wait for interrupt signal to gracefully shutdown the server with a timeout of 5 seconds.
-	// Use a buffered channel to avoid missing signals as recommended for signal.Notify
-	stop := make(chan os.Signal, 1)
-	signal.Notify(stop, os.Interrupt)
-	signal.Notify(stop, syscall.SIGTERM)
-
-	//Recieve shutdown signals.
-	<-stop
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	if err := e.Shutdown(ctx); err != nil {
-		e.Logger.Fatal(err)
-	}
-
 	/*
 		db.Init()
+		defer db.Close()
 		//migration.Init()
+		//elasticsearch.Init()
 
 		e := echo.New()
 		middleware.Init(e)
@@ -100,7 +52,48 @@ func main() {
 		f := factory.NewFactory()
 		http.Init(e, f)
 
-		e.Logger.Fatal(e.Start(":" + PORT))
+		debugpprof := strings.ToLower(strings.TrimSpace(os.Getenv("DEBUG_PPROF")))
+		if debugpprof == "active" {
+			// automatically add routers for net/http/pprof
+			// e.g. /debug/pprof, /debug/pprof/heap, etc.
+			echopprof.Wrap(e)
+			logrus.Info("ECHOPPROF registered")
+		}
+		logrus.Info("DEBUG_PPROF ", debugpprof == "active", " "+debugpprof)
 
+		// Start server
+		go func() {
+			if err := e.Start(":" + PORT); err != nil && err != netHTTP.ErrServerClosed {
+				e.Logger.Fatal("shutting down the server")
+			}
+		}()
+
+		// Wait for interrupt signal to gracefully shutdown the server with a timeout of 5 seconds.
+		// Use a buffered channel to avoid missing signals as recommended for signal.Notify
+		stop := make(chan os.Signal, 1)
+		signal.Notify(stop, os.Interrupt)
+		signal.Notify(stop, syscall.SIGTERM)
+
+		//Recieve shutdown signals.
+		<-stop
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		if err := e.Shutdown(ctx); err != nil {
+			e.Logger.Fatal(err)
+		}
 	*/
+
+	db.Init()
+	//migration.Init()
+
+	e := echo.New()
+	middleware.Init(e)
+
+	f := factory.NewFactory()
+	http.Init(e, f)
+
+	e.Logger.Fatal(e.Start(":" + PORT))
+
 }
